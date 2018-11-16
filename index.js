d77cc78bd886f0a3885369a0f3ec3e3fbd8669ab
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-11-16 23:36:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "Let's talk about Let's talk about Let's talk about space....";
 
-${"Let's talk about"} = ${topic}
-
-var conversation = `${topic}`;
+var conversation = ${"Let's talk about"}
 
