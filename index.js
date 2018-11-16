e1c1f4d406231a0321832c4d2d9db78e6b2e7325
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-11-16 23:42:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "Let's talk about";
 
-var conversation = ${topic} + "Let's talk about";
+var conversation = "Let's talk about" + ${topic};
 
